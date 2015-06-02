641264d2ddd5479c36407f0e5b61a647ae1bcf23
Marcus Besjes <marcusbesjes@gmail.com>
2015-06-02 11:33:55 +0200

    done with intro

--- index.js
+++ index.js
@@ -1,68 +1,79 @@
 window.package = require('package.json')
 require('./style.less')
 
-var Element = require('vigour-js/app/ui/element')
 var timestamp = require('monotonic-timestamp')
-var app = require('vigour-js/app')
-			.inject( require('vigour-js/app/cloud') )
+
+var app = window.a = require('vigour-js/app').inject(
+  require('vigour-js/app/cloud')
+)
 
 app.cloud = 'localhost:10001'
 
-var Todo = new Element({
-	title: {
-		node:'textarea',
-		text: { 
-			data: 'title' 
-		},
-		events: {
-			keyup:function(e) {
-				this.data.set('title', this.node.value)
-			}
-		}
-	},
-	body: {
-		node:'textarea',
-		text: { data: 'text' },
-		events: {
-			keyup:function(e) {
-				this.data.set('text', this.node.value)
-			}
-		}
-	},
-	removeButton: {
-		node:'button',
-		text:'remove',
-		events: {
-			click:function() {
-				this.parent.data.remove()
-			}
-		}
-	}
+var Element = require('vigour-js/app/ui/element')
+
+var vObj = require('vigour-js/object')
+
+var Task = new Element({
+  title: {
+    node: 'textarea',
+    text: { data: 'title' },
+    events: {
+      keyup: function(){
+        var taskdata = this.data
+        taskdata.title.val = this.node.value
+      }
+    }
+  },
+  body: {
+    node:'textarea',
+    text: { data: 'body' },
+    events: {
+      keyup: function(){
+        var taskdata = this.parent.data
+        taskdata.body.val = this.node.value
+      }
+    }
+  },
+  removeButton: {
+    node: 'button',
+    text: 'remove',
+    events: {
+      click: function() {
+        var taskdata = this.parent.data
+        taskdata.remove()
+      }
+    }
+  }
 }).Class
 
 var TodoList = new Element({
-	collection: {
-		data:true,
-		element: Todo
-	}
+  collection: {
+    data: true,
+    element: Task
+  }
 }).Class
 
-//control layer
-var dataObj = window.dataObj = app.cloud.data.get('todos')
+var tasklist = window.tl = app.cloud.data.get('taskList')
+
+tasklist.set('fieldname', {title: 'hey', body: 'ha'})
 
 app.set({
-	addTodo: {
-		node:'button',
-		text:'add a todo!',
-		events: {
-			click:function() {
-				this.parent.data.set(timestamp(), {
-					title:'new todo',
-					text:'describe what you want to do!'
-				})
-			}
-		}
-	},
-	todos: new TodoList(),
-	data: dataObj
+  addTodo: {
+    node: 'button',
+    text: 'add a todo!',
+    events: {
+      click:function() {
+        console.log('this', this)
+        var taskList = this.parent.data
+        taskList.set( timestamp(), {
+          title:'new todo',
+          body:'describe what you want to do!'
+        })
+      }
+    }
+  },
+  todolist: new TodoList(),
+  data: tasklist
 })
+
+
